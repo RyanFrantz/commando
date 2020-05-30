9d7bc1f5373acfea035d0c9d8a3e2f768593cba1
Ryan Frantz <ryanleefrantz@gmail.com>
2020-05-30 19:33:54 +0000

    Misc fixes.

--- mimic_slash_command.sh
+++ mimic_slash_command.sh
@@ -21,11 +21,11 @@ function fail() {
 curl \
   -F "channel_id=DREAMBIG" \
   -F 'token=So12345678asdfghjk' \
-  -F 'team_id=TEAMJELI1' \
+  -F 'team_id=TKVN58V5K' \
   -F 'team_domain=jelitalk' \
-  -F 'channel_id=DREAMBIG' \
+  -F 'channel_id=DKJA3CVU3' \
   -F 'channel_name=directmessage' \
-  -F 'user_id=UFGUTHD2M' \
+  -F 'user_id=UKXHG8T5M' \
   -F 'user_name=ryan' \
   -F 'command=%2Fcommando' \
   -F 'response_url=https%3A%2F%2Fhooks.slack.com%2Fcommands%2FTKVN58V5K%2F1125858804757%2FrFWbh7OK8JYOkzP9QycvKWsZ' \
@@ -33,5 +33,5 @@ curl \
   localhost:5000/run
 }
 
- deploy
-#fail
+#deploy
+fail
