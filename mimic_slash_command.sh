6d1f372868862ae5c4a571060f88640e5286cc5e
Ryan Frantz <ryanleefrantz@gmail.com>
2020-05-30 20:53:45 +0000

    Misc testing command updates.

--- mimic_slash_command.sh
+++ mimic_slash_command.sh
@@ -2,13 +2,12 @@
 
 function deploy() {
 curl \
-  -F "channel_id=DREAMBIG" \
+  -F 'channel_id=DKJA3CVU3' \
   -F 'token=So12345678asdfghjk' \
-  -F 'team_id=TEAMJELI1' \
+  -F 'team_id=TKVN58V5K' \
   -F 'team_domain=jelitalk' \
-  -F 'channel_id=DREAMBIG' \
   -F 'channel_name=directmessage' \
-  -F 'user_id=UFGUTHD2M' \
+  -F 'user_id=UKXHG8T5M' \
   -F 'user_name=ryan' \
   -F 'command=%2Fcommando' \
   -F 'text=deploy' \
@@ -17,9 +16,8 @@ curl \
   localhost:5000/run
 }
 
-function fail() {
+function missing_cmd() {
 curl \
-  -F "channel_id=DREAMBIG" \
   -F 'token=So12345678asdfghjk' \
   -F 'team_id=TKVN58V5K' \
   -F 'team_domain=jelitalk' \
@@ -33,5 +31,39 @@ curl \
   localhost:5000/run
 }
 
+function help() {
+curl \
+  -F 'token=So12345678asdfghjk' \
+  -F 'team_id=TKVN58V5K' \
+  -F 'team_domain=jelitalk' \
+  -F 'channel_id=DKJA3CVU3' \
+  -F 'channel_name=directmessage' \
+  -F 'user_id=UKXHG8T5M' \
+  -F 'user_name=ryan' \
+  -F 'command=%2Fcommando' \
+  -F 'text=help' \
+  -F 'response_url=https%3A%2F%2Fhooks.slack.com%2Fcommands%2FTKVN58V5K%2F1125858804757%2FrFWbh7OK8JYOkzP9QycvKWsZ' \
+  -F 'trigger_id=1139860966177.675753301189.41b8edc5999f487c4b3ddbdc1b69ee52' \
+  localhost:5000/run
+}
+
+function list() {
+curl \
+  -F 'token=So12345678asdfghjk' \
+  -F 'team_id=TKVN58V5K' \
+  -F 'team_domain=jelitalk' \
+  -F 'channel_id=DKJA3CVU3' \
+  -F 'channel_name=directmessage' \
+  -F 'user_id=UKXHG8T5M' \
+  -F 'user_name=ryan' \
+  -F 'command=%2Fcommando' \
+  -F 'text=list' \
+  -F 'response_url=https%3A%2F%2Fhooks.slack.com%2Fcommands%2FTKVN58V5K%2F1125858804757%2FrFWbh7OK8JYOkzP9QycvKWsZ' \
+  -F 'trigger_id=1139860966177.675753301189.41b8edc5999f487c4b3ddbdc1b69ee52' \
+  localhost:5000/run
+}
+
 #deploy
-fail
+#missing_cmd
+#help
+list
